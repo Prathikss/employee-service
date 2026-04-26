356a35178d1746356e52fe5996437aaf3c0abc10
Prathik <prathik.palabhavi@acuverconsulting.com>
2026-04-26 12:01:44 +0530

    Added DeleteBy ID Method

--- src/main/java/com/example/employee/service/EmployeeService.java
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -23,4 +23,8 @@ public class EmployeeService {
     public List<Employee> getAll() {
         return repository.findAll();
     }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
